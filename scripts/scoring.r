98ebdfbeb1ea1e7ab14ea4b9351fc4727688e64a
RacheleRR <124870594+RacheleRR@users.noreply.github.com>
2026-03-25 15:30:39 +0100

    way of scoring variants

---
+++ scripts/scoring.r
@@ -0,0 +1,53 @@
+# FOR SCORING 
+we will use genes that are direct target or transporter of lithium 
+
+## DrugBank
+# URL: https://go.drugbank.com/
+# Search: "lithium" or "lithium carbonate"
+
+
+
+WE will use the genes associated to lithium response found by gwas 
+# URL: https://www.ebi.ac.uk/gwas/
+# Search: "lithium response"
+
+
+# URL: https://www.pharmgkb.org/
+# Search: "lithium"
+
+
+## DisGeNET
+# URL: https://www.disgenet.org/
+# Search: "bipolar disorder" or "lithium response"
+
+## gene ontology
+#search lithium 
+# https://amigo.geneontology.org/amigo/term/GO:0010226
+
+
+We will use genes asscoiated to neurodrugs in general or psychodrugs  in general 
+# URL: https://www.pharmgkb.org/
+# Search: "antipsychotics" "Psychoanaleptics" "antidepressants" "Psycholeptics"
+
+
+We will use genes asscoiated to functional impact oflithium or disorder 
+# we will use genes asscoiated to neurotransmitters
+# we will use genes associated to neuroprotection and plasticity
+# Or maybe we wil use genes asscoiated to neuro and synpases in general 
+
+
+# we will use genes associated to circadian rhythm because lithium is known to affect circadian rhythm and this is thought to be one of the mechanisms of action of lithium
+# GO:0007623
+
+# we will use genes asscoiated to mitocondria function 
+# GO:0140053
+
+# we will use genes associated to oxidative stress 
+# GO:0006979
+
+# we will use genes asscoiated to signaling patways because second messengers are important for lithium response
+# GO:0023052 signaling
+
+
+We will use genes associated to bipolar disorder 
+# found in bipex bd 
